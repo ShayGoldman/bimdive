238d37b550ca61cee91ff0f51dc5cfb2d5efa0d0
Shay Goldman <shay@jolt.io>
2020-10-03 21:54:40 +0300

    test metabase embed

--- packages/interface/pages/home/index.tsx
+++ packages/interface/pages/home/index.tsx
@@ -1,12 +1,31 @@
-import { useSearchParam } from "react-use";
+import axios from "axios";
+import { useSearchParam, useAsync } from "react-use";
 
 export default function HomePage() {
   const email = useSearchParam("emailz");
   const error = useSearchParam("error");
+  const { loading, value } = useAsync(
+    () =>
+      axios.post(
+        "http://ft92wl46ie.execute-api.eu-west-2.amazonaws.com/prod/metabase/embed",
+        { questionId: 17 }
+      ),
+    []
+  );
 
   return (
     <div>
       <h1>Home Page</h1>
+      {value && value.url && (
+        <iframe
+          src={value.url}
+          allowTransparency
+          frameBorder="0"
+          width="800"
+          height="600"
+        />
+      )}
+      {value && <pre>{value}</pre>}
       {email && <h2>Henlo {email}</h2>}
       {error && (
         <section>
