1ccf0b0f50920032b62f56ed58148a9cf4180ae4
Shay Goldman <shay@jolt.io>
2020-10-11 22:14:44 +0300

    added dist folder for rest-api-client (temporary)

---
+++ packages/rest-api-client/dist/models/index.d.ts
@@ -0,0 +1,9 @@
+export * from "./AccessTokens";
+export * from "./CustomAttributes";
+export * from "./IssueComments";
+export * from "./IssueCustomAttributes";
+export * from "./Issues";
+export * from "./Scans";
+export * from "./Users";
+export * from "./VIssueCustomAttributes";
+//# sourceMappingURL=index.d.ts.map
