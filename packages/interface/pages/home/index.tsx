33229f66ed890038f4cfb2fe1fb739bd409f0ffe
talyohai <talko@cervellosec.com>
2020-10-30 18:16:32 +0200

    package.json: Add 'prettier' Add .prettierrc

--- packages/interface/pages/home/index.tsx
+++ packages/interface/pages/home/index.tsx
@@ -1,103 +1,93 @@
-import axios from "axios";
-import { useCallback } from "react";
-import { useAsync, useCookie } from "react-use";
+import axios from 'axios';
+import { useCallback } from 'react';
+import { useAsync, useCookie } from 'react-use';
 
 const apiUrl = `https://ip32mnh28g.execute-api.eu-west-2.amazonaws.com/prod`;
 
-const Spinner = () => (
-  <img src="https://i.imgflip.com/4hkjrq.jpg" width="200" />
-);
+const Spinner = () => <img src="https://i.imgflip.com/4hkjrq.jpg" width="200" />;
 
 const Error = () => <img src="https://i.imgflip.com/4hkjzg.jpg" width="200" />;
 
 const Demo1 = () => {
-  const { loading, value, error } = useAsync(async () => {
-    const { data } = await axios.post(apiUrl + "/metabase/embed", {
-      questionId: 17,
-    });
-    return data;
-  }, []);
+    const { loading, value, error } = useAsync(async () => {
+        const { data } = await axios.post(apiUrl + '/metabase/embed', {
+            questionId: 17,
+        });
+        return data;
+    }, []);
 
-  return (
-    <div>
-      {loading && <Spinner />}
-      {!loading && error && <Error />}
-      {!loading && !error && value?.data?.url && (
-        <iframe src={value.data.url} frameBorder="0" width="400" height="400" />
-      )}
-    </div>
-  );
+    return (
+        <div>
+            {loading && <Spinner />}
+            {!loading && error && <Error />}
+            {!loading && !error && value?.data?.url && <iframe src={value.data.url} frameBorder="0" width="400" height="400" />}
+        </div>
+    );
 };
 
 const Demo2 = () => {
-  const { loading, value, error } = useAsync(async () => {
-    const { data } = await axios.post(apiUrl + "/metabase/embed", {
-      questionId: 19,
-    });
-    return data;
-  }, []);
+    const { loading, value, error } = useAsync(async () => {
+        const { data } = await axios.post(apiUrl + '/metabase/embed', {
+            questionId: 19,
+        });
+        return data;
+    }, []);
 
-  return (
-    <div>
-      <h1>Home Page Depo</h1>
-      {value?.data?.url && (
-        <iframe
-          src={value.data.url}
-          allowTransparency
-          frameBorder="0"
-          width="800"
-          height="600"
-        />
-      )}
-      {error && (
-        <section>
-          <h2>Error</h2>
-          <pre>{error}</pre>
-        </section>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Home Page Depo</h1>
+            {value?.data?.url && <iframe src={value.data.url} allowTransparency frameBorder="0" width="800" height="600" />}
+            {error && (
+                <section>
+                    <h2>Error</h2>
+                    <pre>{error}</pre>
+                </section>
+            )}
+        </div>
+    );
 };
 
 const ScanButton = ({ email }) => {
-  const [cookie] = useCookie("_bimdive");
+    const [cookie] = useCookie('_bimdive');
 
-  const scan = useCallback(async () => {
-    const { id } = JSON.parse(cookie);
-    if (id) {
-      await axios.post(apiUrl + "/scan", { userId: id });
-    }
-  }, [email]);
+    const scan = useCallback(async () => {
+        console.log(cookie);
 
-  return (
-    <button style={{ cursor: "pointer" }} onClick={scan}>
-      Scan for projects
-    </button>
-  );
+        const { id } = JSON.parse(cookie);
+        if (id) {
+            await axios.post(apiUrl + '/scan', { userId: id });
+        }
+    }, [email]);
+
+    return (
+        <button style={{ cursor: 'pointer' }} onClick={scan}>
+            Scan for projects
+        </button>
+    );
 };
 
 export default function HomePage({ query }) {
-  const { email = "" } = query;
-  return (
-    <div>
-      <h1>Henlo {email}</h1>
-      <h2>Clickies</h2>
-      <ScanButton email={email} />
-      <h2>Demoes</h2>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <Demo1 />
-        <Demo2 />
-      </div>
-    </div>
-  );
+    const { email = '' } = query;
+    return (
+        <div>
+            <h1>Henlo {email}</h1>
+            <h2>Clickies</h2>
+            <ScanButton email={email} />
+            <h2>Demoes</h2>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                }}
+            >
+                <Demo1 />
+                <Demo2 />
+            </div>
+        </div>
+    );
 }
 
 HomePage.getInitialProps = ({ query }) => {
-  return { query };
+    return { query };
 };
