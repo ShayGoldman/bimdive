b9378efce61aa9045f3d573c9db107640db959ee
talyohai <talko@cervellosec.com>
2020-11-15 00:02:50 +0200

    next.config.js: Update comment

--- packages/interface/next.config.js
+++ packages/interface/next.config.js
@@ -5,7 +5,7 @@ module.exports = withStyles({
     modules: true, // style.(m|module).css & style.(m|module).scss for module files
     sassLoaderOptions: {
         sassOptions: {
-            includePaths: ['styles'], // @import 'variables'; # loads (src/styles/varialbes.scss), you got it..
+            includePaths: ['styles'], // @import 'variables'; # loads (styles/variables.scss)
         },
     },
 });
