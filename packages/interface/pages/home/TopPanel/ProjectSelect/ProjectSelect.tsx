abd45b4daa002f6c2fa287e6fa566f237f47cd41
talyohai <talko@cervellosec.com>
2020-11-15 00:53:40 +0200

    scans.graphql: Renamed from  'events-scans.graphql' ProjectSelect.tsx: Replace 'useEventsScansQuery' with 'useScansQuery'

--- packages/interface/pages/home/TopPanel/ProjectSelect/ProjectSelect.tsx
+++ packages/interface/pages/home/TopPanel/ProjectSelect/ProjectSelect.tsx
@@ -1,20 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import CustomSelect from '../../../../components/CustomSelect/CustomSelect';
-import { useEventsScansQuery } from '../../../../schema/generated/graphql';
 import ErrorMessage from '../../../../components/ErrorMessage/ErrorMessage';
+import { useScansQuery } from '../../../../schema/generated/graphql';
 import './ProjectSelect.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
 const ProjectSelect: FunctionComponent<Props> = props => {
-    const { error, data } = useEventsScansQuery();
+    const { error, data } = useScansQuery();
 
     if (error) return <ErrorMessage error={error} />;
 
     const options = !data
         ? []
-        : data.events_scans.map(({ project_provider_id, project_name }) => ({ key: project_provider_id, label: project_name }));
+        : data.scans.map(({ project_provider_id, project_name }) => ({ key: project_provider_id, label: project_name }));
 
     return (
         <CustomSelect options={options} value={options[0]?.key || ''} onValueChange={newValue => console.log(newValue)} className="project-select" />
